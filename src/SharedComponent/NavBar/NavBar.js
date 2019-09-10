15d8359a94d4503d964be637bb55144a3d8558c8
Islam <islam.elsharkawy@hotmail.com>
2019-09-10 21:42:19 +0200

    Create Logo & NavBarRight

--- src/SharedComponent/NavBar/NavBar.js
+++ src/SharedComponent/NavBar/NavBar.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div>Bonjour</div>;
+  return (
+    <nav className="NavBar">
+      {/* LOGO A GAUCHE  */}
+      <Logo />
+      {/* nav bar à droite */}
+      <NavBarRight />
+    </nav>
+  );
+};
+
+const Logo = () => {
+  return (
+    <divlogo className="Logo">
+      <span className="Eco">E-co</span>
+      <span className="Working">Working</span>
+    </divlogo>
+  );
+};
+
+const NavBarRight = () => {
+  return (
+    <divrightnav className="NavBarRight">
+      <button>
+        <p>Espaces</p>
+      </button>
+      <button>
+        <p>Around Me</p>
+      </button>
+      <button>
+        <p>Sign Up</p>
+      </button>
+      <button>
+        <p>Search</p>
+      </button>
+      <button>
+        <p>Mail</p>
+      </button>
+    </divrightnav>
+  );
 };
 
 export default NavBar;
