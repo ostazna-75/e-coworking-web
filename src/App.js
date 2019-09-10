7500ffbd81bc5a9ac48787cd0c88463a94b3343b
Islam <islam.elsharkawy@hotmail.com>
2019-09-10 19:40:13 +0200

    deleat old app.css + app.js null

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return null;
 }
 
 export default App;
