7bb1464bb1d2ea7add97d05c0d6c063e126f9cb8
Islam <islam.elsharkawy@hotmail.com>
2019-09-10 20:46:04 +0200

    Create Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./SharedComponent/NavBar/NavBar";
 
 function App() {
-  return null;
+  return (
+    <div className="MainContainer">
+      <NavBar />
+      <p>salut</p>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/SharedComponent/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div>Bonjour</div>;
+};
+
+export default NavBar;
