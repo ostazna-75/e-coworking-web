3aa98f7769cb42386539730001834df37b149fb3
gashmal <tovonandroy@gmail.com>
2019-09-14 09:35:44 +0200

    Refacto(SlideOne): add paragraph

--- src/Views/1-Landing/SlideOne.js
+++ src/Views/1-Landing/SlideOne.js
@@ -5,48 +5,48 @@ import Container from "../../SharedComponent/Container";
 import LandingButton from "./Components/LandingButton";
 
 const SlideOne = () => {
-  return (
-    <div className="SlideOne">
-      <Container>
-        <NavBar />
-        {/* <p>salut</p> */}
-        {/* slide E-Co WorkING - Ecriture SLide One */}
-        <divlogoslide className="LogoSlide">
-          <span className="Eslide">E</span>
-          <span className="TiréSlide">-</span>
-          <span className="CoSlide">co</span>
-          <br />
-          <span className="Wslide">W</span>
-          <span className="OrkingSlide">orkING</span>
-        </divlogoslide>
-        {/* Fin slide E-Co WorkING - SLide One */}
-        {/* Explication sous E-co Working SLIDE */}
-        <divword className="wordeco">
-          <p className="para-eco">
-            Que vous soyez un élève,
-            <br /> une startup , une association ou une grande
-            <br />
-            entreprise en développement, découvrez des espaces
-            <br /> qui permettront de faire un travail qui a du sens et un
-            impact.
-          </p>
-        </divword>
-
-        <Buttons>
-          <LandingButton title="Découvrez les Espaces" />
-          <LandingButton title="Map" />
-        </Buttons>
-      </Container>
-      {/* Explication sous E-co Working SLIDE */}
-    </div>
-  );
+	return (
+		<div className="SlideOne">
+			<Container>
+				<NavBar />
+				<divlogoslide className="LogoSlide">
+					<span className="Eslide">E</span>
+					<span className="TiréSlide">-</span>
+					<span className="CoSlide">co</span>
+					<br />
+					<span className="Wslide">W</span>
+					<span className="OrkingSlide">orkING</span>
+				</divlogoslide>
+				<Paragraph>
+					<p>
+						Que vous soyez un élève,
+						<br /> une startup , une association ou une grande
+						<br />
+						entreprise en développement, découvrez des espaces
+						<br /> qui permettront de faire un travail qui a du sens et un
+						impact.
+					</p>
+				</Paragraph>
+				<Buttons>
+					<LandingButton title="Découvrez les Espaces" />
+					<LandingButton title="Map" />
+				</Buttons>
+			</Container>
+		</div>
+	);
 };
 
+const Paragraph = styled.div`
+	font-family: "SFUIDisplay";
+	color: white;
+	font-size: 17px;
+`;
+
 const Buttons = styled.div`
-  margin-top: 40px;
-  display: grid;
-  grid-template-columns: auto auto 1fr;
-  grid-column-gap: 20px;
+	margin-top: 40px;
+	display: grid;
+	grid-template-columns: auto auto 1fr;
+	grid-column-gap: 20px;
 `;
 
 export default SlideOne;
