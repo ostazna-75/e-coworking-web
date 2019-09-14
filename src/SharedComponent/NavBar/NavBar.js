e3eacbacb375f2e4a68b458e708716bf914e4f6b
Islam <islam.elsharkawy@hotmail.com>
2019-09-14 10:37:55 +0200

    Nav BAr CSS

--- src/SharedComponent/NavBar/NavBar.js
+++ src/SharedComponent/NavBar/NavBar.js
@@ -4,43 +4,43 @@ import NavButton from "./Components/NavButton";
 import Logo from "../Logo";
 
 const NavBar = () => {
-	return (
-		<NavStyleBar>
-			{/* LOGO A GAUCHE  */}
-			<Logo />
-			{/* nav bar à droite */}
-			<NavBarRight />
-		</NavStyleBar>
-	);
+  return (
+    <NavStyleBar>
+      {/* LOGO A GAUCHE  */}
+      <Logo />
+      {/* nav bar à droite */}
+      <NavBarRight />
+    </NavStyleBar>
+  );
 };
 
 /* Navigation bar Droite  */
 const NavBarRight = () => {
-	return (
-		<RightNav>
-			<NavButton title="Espaces" />
-			<NavButton title="Around Me" />
-			<NavButton title="Sign UP" />
-			<NavButton title="Search" />
-			<NavButton title="Mail" />
+  return (
+    <RightNav>
+      <NavButton title="Espaces" />
+      <NavButton title="Around Me" />
+      <NavButton title="Sign UP" />
+      <NavButton icon="Search" />
+      <NavButton icon="Mail" title="Tovo" />
 
-			<NavButton />
-		</RightNav>
-	);
+      <NavButton />
+    </RightNav>
+  );
 };
 
 /* STYLE CSS Nouvelle méthode : styled.component */
 /* bar de navigation droite */
 const NavStyleBar = styled.nav`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	margin-top: 1%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 1%;
 `;
 
 const RightNav = styled.div`
-	display: flex;
-	flex-direction: row;
+  display: flex;
+  flex-direction: row;
 `;
 
 /* Discover and MAP */
