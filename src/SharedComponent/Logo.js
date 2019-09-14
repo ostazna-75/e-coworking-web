148d0c59ec2c23d3e8299b6bf042cc70a8a87741
gashmal <tovonandroy@gmail.com>
2019-09-14 09:33:56 +0200

    Refacto(Logo): extract Logo

---
+++ src/SharedComponent/Logo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+/* Logo E-co Working */
+const Logo = () => {
+	return (
+		<LogoNav>
+			<Eco>E</Eco>
+			<span className="Tiré">-</span>
+			<span className="Co">co</span>
+			<span className="W">W</span>
+			<span className="orking">orkING</span>
+		</LogoNav>
+	);
+};
+
+const LogoNav = styled.div`
+	color: white;
+`;
+
+const Eco = styled.span`
+	font-family: "FallisComing";
+	font-size: 24pt;
+`;
+
+export default Logo;
