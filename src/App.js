59d67724834d38aedc72225e78b3cc9bd1d0d889
Islam <islam.elsharkawy@hotmail.com>
2019-09-15 12:22:58 +0200

    modal for Signup

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import SlideTwo from "./Views/2-Welcome/SlideTwo";
 import NavBar from "./SharedComponent/NavBar/NavBar";
 import Modal from "./SharedComponent/Modal";
 import useModal from "./Hooks/useModal";
+import SignUp from "./Views/8-SignUp/SignUp";
 
 function App() {
-	const { isShowing, toggle } = useModal();
+  const { isShowing, toggle } = useModal();
 
-	return (
-		<div style={{ position: "relative" }}>
-			<Modal isShowing={isShowing} hide={toggle}>
-				yooooooooooooo
-			</Modal>
-			<NavBar />
-			<SlideOne />
-			<SlideTwo /> <button onClick={toggle}>ouiououoi</button>
-		</div>
-	);
+  return (
+    <div style={{ position: "relative" }}>
+      <Modal isShowing={isShowing} hide={toggle}>
+        <SignUp />
+      </Modal>
+      <NavBar />
+      <SlideOne />
+      <SlideTwo /> <button onClick={toggle}>ouiououoi</button>
+    </div>
+  );
 }
 
 export default App;
--- src/SharedComponent/Modal.js
+++ src/SharedComponent/Modal.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 
 const Modal = ({ isShowing, hide, children }) =>
-	isShowing
-		? ReactDOM.createPortal(
-				<StyledModal onClick={hide}>{children}</StyledModal>,
-				document.body
-		  )
-		: null;
+  isShowing
+    ? ReactDOM.createPortal(
+        <StyledModal>{children}</StyledModal>,
+        document.body
+      )
+    : null;
 
 const StyledModal = styled.div`
-	position: absolute;
-	height: 100vh;
-	width: 100%;
-	background-color: red;
+  position: absolute;
+  height: 100vh;
+  width: 100%;
+  background-color: white;
 `;
 
 export default Modal;
