d89f410a8e91b43defe8351d96377d4aedff4e37
Islam <islam.elsharkawy@hotmail.com>
2019-09-24 21:11:35 +0200

    Create new props for NavButton.js

--- src/SharedComponent/NavBar/Components/NavButton.js
+++ src/SharedComponent/NavBar/Components/NavButton.js
@@ -4,49 +4,49 @@ import search from "../../../Assets/Images/search.png";
 import mail from "../../../Assets/Images/mail.png";
 
 const NavButton = ({ title = "", icon = "", action = () => {} }) => {
-	if (icon !== "") {
-		if (icon === "mail") {
-			return (
-				<div>
-					<a href="mailto:islam.elsharkawy@hotmail.com">
-						<StyleIcon src={mail} />
-					</a>
-					;{/* ? : si icon est soarch sinon mail (if else) */}
-				</div>
-			);
-		} else {
-			return (
-				<div onClick={action}>
-					<StyleIcon src={search} />;
-					{/* ? : si icon est soarch sinon mail (if else) */}
-				</div>
-			);
-		}
-	} else {
-		return (
-			<StyleButton onClick={action}>
-				<p>{title}</p>
-			</StyleButton>
-		);
-	}
+  if (icon !== "") {
+    if (icon === "mail") {
+      return (
+        <div>
+          <a href="mailto:islam.elsharkawy@hotmail.com">
+            <StyleIcon src={mail} />
+          </a>
+          ;{/* ? : si icon est soarch sinon mail (if else) */}
+        </div>
+      );
+    } else {
+      return (
+        <div onClick={action}>
+          <StyleIcon src={search} />;
+          {/* ? : si icon est soarch sinon mail (if else) */}
+        </div>
+      );
+    }
+  } else {
+    return (
+      <StyleButton onClick={action}>
+        <p>{title}</p>
+      </StyleButton>
+    );
+  }
 };
 
 /* style des button */
 const StyleButton = styled.button`
-	font-family: "SFUIDisplay";
-	color: white;
-	background-color: transparent;
-	border: transparent;
-	p:hover {
-		color: #ffe699;
-		cursor: pointer;
-	}
+  font-family: "SFUIDisplay";
+  color: white;
+  background-color: transparent;
+  border: transparent;
+  p:hover {
+    color: #ffe699;
+    cursor: pointer;
+  }
 `;
 
 /* style des icones */
 const StyleIcon = styled.img`
-	width: 20px;
-	padding-left: 15px;
+  width: 20px;
+  padding-left: 15px;
 `;
 /* si icone différent !=== "", alors on affiche icone n sinon le texte */
 
