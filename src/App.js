46696077e4c8e17fde81097a5b125a6e4f3772e3
gashmal <tovonandroy@gmail.com>
2019-09-28 13:30:20 +0200

    custom hooks c'est le turfu

--- src/App.js
+++ src/App.js
@@ -16,65 +16,68 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
-  const { isShowing, toggle } = useModal();
-  const [user, setUser] = useState(
-    JSON.parse(localStorage.getItem("User")) || { username: "" }
-  );
-  const verifyToken = async () => {
-    // do the axios, await the response
-    try {
-      await axios.post("user/verification", {
-        email: user.email,
-        token: user.token
-      });
-      // ensuite if "c'est bon frère" => on fait rien
-    } catch (error) {
-      // else if "y'a carotte" => on se déconnecte
-      if (user.username !== "") setUser({ username: "" });
-    }
-  };
+	const { isShowing, toggle } = useModal();
+	const { user, setUser } = useUser();
 
-  useEffect(() => {
-    localStorage.setItem("User", JSON.stringify(user));
-    verifyToken();
-    return () => {};
-  }, [user]);
-  /* React.useState(
-    localStorage.getItem('myValueInLocalStorage') || ''
-  ); */
-
-  return (
-    <div style={{ position: "relative" }}>
-      {/* en dehors du router */}
-      <Modal isShowing={isShowing}>
-        <SignUp hide={toggle} setUser={setUser} />
-      </Modal>
-      <NavBar toggle={toggle} username={user.username} setUser={setUser} />
-      {/* la ou on va mettre les pages: pages 1, pages 2, pages3, pages4 */}
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Landing
-              toggle={toggle}
-              username={user.username}
-              setUser={setUser}
-            />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div style={{ position: "relative" }}>
+			{/* en dehors du router */}
+			<Modal isShowing={isShowing}>
+				<SignUp hide={toggle} setUser={setUser} />
+			</Modal>
+			<NavBar toggle={toggle} username={user.username} setUser={setUser} />
+			{/* la ou on va mettre les pages: pages 1, pages 2, pages3, pages4 */}
+			<Router>
+				<Switch>
+					<Route path="/">
+						<Landing
+							toggle={toggle}
+							username={user.username}
+							setUser={setUser}
+						/>
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
+const useUser = () => {
+	const [user, setUser] = useState(
+		JSON.parse(localStorage.getItem("User")) || { username: "" }
+	);
+	const verifyToken = async () => {
+		// do the axios, await the response
+		try {
+			await axios.post("user/verification", {
+				email: user.email,
+				token: user.token
+			});
+			// ensuite if "c'est bon frère" => on fait rien
+		} catch (error) {
+			// else if "y'a carotte" => on se déconnecte
+			if (user.username !== "") setUser({ username: "" });
+		}
+	};
+
+	useEffect(() => {
+		localStorage.setItem("User", JSON.stringify(user));
+		verifyToken();
+		return () => {};
+	}, [user]);
+
+	return { user, setUser };
+};
+
 const Landing = ({ toggle, username, setUser }) => {
-  return (
-    <>
-      <SlideOne toggle={toggle} username={username} setUser={setUser} />
-      <SlideTwo />
-      <SlideThree />
-      <ProductCategories />
-    </>
-  );
+	return (
+		<>
+			<SlideOne toggle={toggle} username={username} setUser={setUser} />
+			<SlideTwo />
+			<SlideThree />
+			<ProductCategories />
+		</>
+	);
 };
 
 export default App;
