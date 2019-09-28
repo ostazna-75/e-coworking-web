4b5e617a45da6ba1aede9f04ff928bb381b87eeb
Islam <islam.elsharkawy@hotmail.com>
2019-09-28 14:48:02 +0200

    Create file Presentation.js

---
+++ src/Views/9-Presentation/Presentation.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "../../SharedComponent/Container";
+
+const Presentation = () => {
+  return <Container style={{ marginTop: "100px" }}>Presentation</Container>;
+};
+
+export default Presentation;
