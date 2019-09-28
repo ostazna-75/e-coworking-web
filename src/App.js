eae38741cc821753b583343dd38403dc1342460e
Islam <islam.elsharkawy@hotmail.com>
2019-09-28 14:48:58 +0200

    import Presentation.js in App.js

--- src/App.js
+++ src/App.js
@@ -11,73 +11,83 @@ import { useState, useEffect } from "react";
 import ProductCategories from "./Views/4-Categories/ProductCategories";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from "axios";
+import Presentation from "./Views/9-Presentation/Presentation";
 
 // url de base pour toutes les requetes axios
 axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
-	const { isShowing, toggle } = useModal();
-	const { user, setUser } = useUser();
+  const { isShowing, toggle } = useModal();
+  const { user, setUser } = useUser();
 
-	return (
-		<div style={{ position: "relative" }}>
-			{/* en dehors du router */}
-			<Modal isShowing={isShowing}>
-				<SignUp hide={toggle} setUser={setUser} />
-			</Modal>
-			<NavBar toggle={toggle} username={user.username} setUser={setUser} />
-			{/* la ou on va mettre les pages: pages 1, pages 2, pages3, pages4 */}
-			<Router>
-				<Switch>
-					<Route path="/">
-						<Landing
-							toggle={toggle}
-							username={user.username}
-							setUser={setUser}
-						/>
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div style={{ position: "relative" }}>
+      {/* en dehors du router */}
+      <Modal isShowing={isShowing}>
+        <SignUp hide={toggle} setUser={setUser} />
+      </Modal>
+      <NavBar toggle={toggle} username={user.username} setUser={setUser} />
+      {/* la ou on va mettre les pages: pages 1, pages 2, pages3, pages4 */}
+      <Router>
+        {/* permet d'aller a d'autre page */}
+        <Switch>
+          {/* suivant ou tu est , mettre une route */}
+          {/* Route = 1 route, 1 passage de base : "/"  -------  */}
+          {/* Route de la Landing */}
+          <Route exact path="/">
+            <Landing
+              toggle={toggle}
+              username={user.username}
+              setUser={setUser}
+            />
+          </Route>
+          {/* Route Presentation */}
+          <Route path="/Presentation">
+            <Presentation />
+          </Route>
+          {/* ------------------------ */}
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 const useUser = () => {
-	const [user, setUser] = useState(
-		JSON.parse(localStorage.getItem("User")) || { username: "" }
-	);
-	const verifyToken = async () => {
-		// do the axios, await the response
-		try {
-			await axios.post("user/verification", {
-				email: user.email,
-				token: user.token
-			});
-			// ensuite if "c'est bon frère" => on fait rien
-		} catch (error) {
-			// else if "y'a carotte" => on se déconnecte
-			if (user.username !== "") setUser({ username: "" });
-		}
-	};
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("User")) || { username: "" }
+  );
+  const verifyToken = async () => {
+    // do the axios, await the response
+    try {
+      await axios.post("user/verification", {
+        email: user.email,
+        token: user.token
+      });
+      // ensuite if "c'est bon frère" => on fait rien
+    } catch (error) {
+      // else if "y'a carotte" => on se déconnecte
+      if (user.username !== "") setUser({ username: "" });
+    }
+  };
 
-	useEffect(() => {
-		localStorage.setItem("User", JSON.stringify(user));
-		verifyToken();
-		return () => {};
-	}, [user]);
+  useEffect(() => {
+    localStorage.setItem("User", JSON.stringify(user));
+    verifyToken();
+    return () => {};
+  }, [user]);
 
-	return { user, setUser };
+  return { user, setUser };
 };
 
 const Landing = ({ toggle, username, setUser }) => {
-	return (
-		<>
-			<SlideOne toggle={toggle} username={username} setUser={setUser} />
-			<SlideTwo />
-			<SlideThree />
-			<ProductCategories />
-		</>
-	);
+  return (
+    <>
+      <SlideOne toggle={toggle} username={username} setUser={setUser} />
+      <SlideTwo />
+      <SlideThree />
+      <ProductCategories />
+    </>
+  );
 };
 
 export default App;
