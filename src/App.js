728939ea1bb081eed6e8ce8cefc94c89c309824d
Islam <islam.elsharkawy@hotmail.com>
2019-09-28 12:49:03 +0200

    localhost: 3001 : Back and front

--- src/App.js
+++ src/App.js
@@ -13,68 +13,68 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from "axios";
 
 // url de base pour toutes les requetes axios
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
-	const { isShowing, toggle } = useModal();
-	const [user, setUser] = useState(
-		JSON.parse(localStorage.getItem("User")) || { username: "" }
-	);
-	const verifyToken = async () => {
-		// do the axios, await the response
-		try {
-			let response = await axios.post("user/verification", {
-				email: user.email,
-				token: user.token
-			});
-			// ensuite if "c'est bon frère" => on fait rien
-		} catch (error) {
-			// else if "y'a carotte" => on se déconnecte
-			if (user.username !== "") setUser({ username: "" });
-		}
-	};
+  const { isShowing, toggle } = useModal();
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("User")) || { username: "" }
+  );
+  const verifyToken = async () => {
+    // do the axios, await the response
+    try {
+      await axios.post("user/verification", {
+        email: user.email,
+        token: user.token
+      });
+      // ensuite if "c'est bon frère" => on fait rien
+    } catch (error) {
+      // else if "y'a carotte" => on se déconnecte
+      if (user.username !== "") setUser({ username: "" });
+    }
+  };
 
-	useEffect(() => {
-		localStorage.setItem("User", JSON.stringify(user));
-		verifyToken();
-		return () => {};
-	}, [user]);
-	/* React.useState(
+  useEffect(() => {
+    localStorage.setItem("User", JSON.stringify(user));
+    verifyToken();
+    return () => {};
+  }, [user]);
+  /* React.useState(
     localStorage.getItem('myValueInLocalStorage') || ''
   ); */
 
-	return (
-		<div style={{ position: "relative" }}>
-			{/* en dehors du router */}
-			<Modal isShowing={isShowing}>
-				<SignUp hide={toggle} setUser={setUser} />
-			</Modal>
-			<NavBar toggle={toggle} username={user.username} setUser={setUser} />
-			{/* la ou on va mettre les pages: pages 1, pages 2, pages3, pages4 */}
-			<Router>
-				<Switch>
-					<Route path="/">
-						<Landing
-							toggle={toggle}
-							username={user.username}
-							setUser={setUser}
-						/>
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div style={{ position: "relative" }}>
+      {/* en dehors du router */}
+      <Modal isShowing={isShowing}>
+        <SignUp hide={toggle} setUser={setUser} />
+      </Modal>
+      <NavBar toggle={toggle} username={user.username} setUser={setUser} />
+      {/* la ou on va mettre les pages: pages 1, pages 2, pages3, pages4 */}
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Landing
+              toggle={toggle}
+              username={user.username}
+              setUser={setUser}
+            />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 const Landing = ({ toggle, username, setUser }) => {
-	return (
-		<>
-			<SlideOne toggle={toggle} username={username} setUser={setUser} />
-			<SlideTwo />
-			<SlideThree />
-			<ProductCategories />
-		</>
-	);
+  return (
+    <>
+      <SlideOne toggle={toggle} username={username} setUser={setUser} />
+      <SlideTwo />
+      <SlideThree />
+      <ProductCategories />
+    </>
+  );
 };
 
 export default App;
