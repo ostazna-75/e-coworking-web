663b770c011de81eb08a57512752197c6ec5ce79
Islam <islam.elsharkawy@hotmail.com>
2019-09-29 10:07:28 +0200

    uselocation, useHIstory in Presentation.js

--- src/Views/9-Presentation/Presentation.js
+++ src/Views/9-Presentation/Presentation.js
@@ -1,8 +1,29 @@
 import React from "react";
 import Container from "../../SharedComponent/Container";
+import { useLocation, useHistory } from "react-router-dom";
 
 const Presentation = () => {
-  return <Container style={{ marginTop: "100px" }}>Presentation</Container>;
+  const location = useLocation();
+  const history = useHistory();
+  console.log({ location });
+
+  return (
+    <>
+      <Container
+        onClick={() => history.push("/")}
+        style={{ marginTop: "100px" }}
+      >
+        Composant avec image
+      </Container>
+
+      <Container
+        onClick={() => history.push("/")}
+        style={{ marginTop: "100px" }}
+      >
+        composants avec tout les deuxième pages après image catégorie
+      </Container>
+    </>
+  );
 };
 
 export default Presentation;
