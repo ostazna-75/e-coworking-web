eb209e9137d8835ebfcbbdb73ee06b016d35bbac
Islam <islam.elsharkawy@hotmail.com>
2019-10-07 21:09:12 +0200

    Read a API in our front ( Presentation.js)
    Container ou va être affiché tous les Coworking

--- src/Views/9-Presentation/Presentation.js
+++ src/Views/9-Presentation/Presentation.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "../../SharedComponent/Container";
 import { useLocation, useHistory } from "react-router-dom";
 import ContentImage from "./ContentImage";
 import styled from "styled-components";
 import espaces from "../4-Categories/Image/1-espaces.jpg";
+import avatar from "./Image/Icons/avatar.png";
+import iconlocation from "./Image/Icons/location.png";
+import DraftCowork from "./Image/draft.jpg";
+import axios from "axios";
 
 const Presentation = () => {
   const location = useLocation();
   const history = useHistory();
   console.log({ location });
 
+  const { list } = useData();
+
+  console.log("list", list);
+
   return (
     <>
       <ContentImage />
-      <Container style={{ marginTop: "100px" }}>
+      <Container>
         <UpContainer>
           <LeftContent>
             {Cats.map(({ src }) => (
@@ -29,77 +37,187 @@ const Presentation = () => {
             <Option>Vous souhaitez plus d'options ?</Option>
             {/* FILTRE */}
             <FilterGroup>
-              <Filter>
-                {filter.map(({ title }) => (
-                  /* METTRE LE ONCLICK DANS LE CARD VERS PRESENTATION */
-                  /* card : les catégories */
-                  <TextFilt onClick={() => history.push("/ProductCategories")}>
-                    {title}
-                  </TextFilt>
-                ))}
-              </Filter>
-              <Filter>
-                {filter2.map(({ title }) => (
-                  /* METTRE LE ONCLICK DANS LE CARD VERS PRESENTATION */
-                  /* card : les catégories */
-                  <TextFilt onClick={() => history.push("/ProductCategories")}>
-                    {title}
-                  </TextFilt>
-                ))}
-              </Filter>
-              <Filter>
-                {filter3.map(({ title }) => (
-                  /* METTRE LE ONCLICK DANS LE CARD VERS PRESENTATION */
-                  /* card : les catégories */
-                  <TextFilt onClick={() => history.push("/ProductCategories")}>
-                    {title}
-                  </TextFilt>
-                ))}
-              </Filter>
+              {filters.map(f => (
+                <Filter>
+                  {f.map(({ title }) => (
+                    /* METTRE LE ONCLICK DANS LE CARD VERS PRESENTATION */
+                    /* card : les catégories */
+                    <TextFilt
+                      onClick={() => history.push("/ProductCategories")}
+                    >
+                      {title}
+                    </TextFilt>
+                  ))}
+                </Filter>
+              ))}
             </FilterGroup>
           </RightContent>
         </UpContainer>
+        <SpaceWorking>
+          <Space>Espaces</Space>
+          <Working>WorkING.</Working>
+        </SpaceWorking>
+        <SpaceListing>
+          {/* list.map : permet de récupérer les donée de l'api */}
+          {/* paramètre name etc.. , les objets à récupérer  */}
+          {list.map(({ name, squaremeters: surface, type }) => (
+            <Cowork>
+              {/* DRaftCowork est l'image */}
+              <Image src={DraftCowork} />
+              <Description>
+                <Name>{name}</Name>
+
+                <Adress>
+                  <img src={iconlocation} /> {type}
+                </Adress>
+                <Capacity>
+                  <img src={avatar} />
+                  {surface}
+                </Capacity>
+              </Description>
+            </Cowork>
+          ))}
+        </SpaceListing>
       </Container>
     </>
   );
 };
 
+const useData = () => {
+  const [list, setList] = useState([]);
+  /* nouvelle fonction pour custom Hooks */
+  const getList = async () => {
+    let response = {};
+    /* getList : lire la liste du titre cowork */
+
+    try {
+      response = await axios.get(`cowork/list`);
+      // toujours mettre sur le code front axios.get avec http au début
+
+      setList(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  return { list };
+};
+
+/* ----------------------- */
+/* Style Presentation Page */
+/* ----------------------- */
+
+/* SpaceListing container sous Espace WorkING. */
+const SpaceListing = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const Cowork = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 260px;
+  width: 250px;
+  border: 1px solid #ffe699;
+  border-radius: 5%;
+  /* background-color: orange; */
+  margin-top: 20px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  height: 155px;
+  width: 250px;
+`;
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Name = styled.div`
+  font-family: "SFUIDisplayMedium";
+  font-size: 12pt;
+  margin-top: 15px;
+  margin-left: 10px;
+  color: #359b80;
+`;
+
+const Adress = styled.div`
+  display: flex;
+  align-items: center;
+  font-family: "SFUIDisplayMedium";
+  font-size: 9pt;
+  margin-top: 15px;
+  margin-left: 10px;
+  color: #236857;
+  font-weight: bold;
+  img {
+    height: 15px;
+    width: 15px;
+    margin-right: 10px;
+  }
+`;
+
+const Capacity = styled.div`
+  display: flex;
+  align-items: center;
+  font-family: "SFUIDisplayMedium";
+  font-size: 9pt;
+  margin-top: 10px;
+  margin-left: 10px;
+  color: #7f6000;
+  img {
+    height: 15px;
+    width: 15px;
+    margin-right: 10px;
+  }
+`;
+
+/* ----------------------------------------------- */
+/* Catégorie src espaces */
 const Cats = [{ src: espaces }];
 
 /* catégorie filter */
-const filter = [
-  { title: "Gratuit" },
-  { title: "Moins de 5 €/ heure" },
-  { title: "Entre 5 et 10 € / heure" },
-  { title: "+ de 10 € / heure" },
-  { title: "+ 20 € / heure" }
-];
+const filters = [
+  [
+    { title: "Gratuit" },
+    { title: "Moins de 5 €/ heure" },
+    { title: "Entre 5 et 10 € / heure" },
+    { title: "+ de 10 € / heure" },
+    { title: "+ 20 € / heure" }
+  ],
+  [
+    {
+      title: "0 à 50 m² de surface"
+    },
+    {
+      title: "50 à 100 m² de surface"
+    },
+    {
+      title: "100 à 200 m² de surface"
+    },
+    {
+      title: "+200 m² de surface"
+    }
+  ],
 
-const filter2 = [
-  {
-    title: "0 à 50 m² de surface"
-  },
-  {
-    title: "50 à 100 m² de surface"
-  },
-  {
-    title: "100 à 200 m² de surface"
-  },
-  {
-    title: "+200 m² de surface"
-  }
-];
-
-const filter3 = [
-  {
-    title: "Salle de réuinion"
-  },
-  {
-    title: "24/7"
-  },
-  {
-    title: "Ouvert le WeekEnd"
-  }
+  [
+    {
+      title: "Salle de réuinion"
+    },
+    {
+      title: "24/7"
+    },
+    {
+      title: "Ouvert le WeekEnd"
+    }
+  ]
 ];
 
 const FilterGroup = styled.div``;
@@ -161,12 +279,12 @@ box-shadow: 7px 4px 58px -41px rgba(0,0,0,0.75);
 
 const UpContainer = styled.div`
   display: flex;
-  flex-grow: 1;
+  /*  flex-grow: 1; */
   flex-direction: row;
+  margin-top: 80px;
 `;
 
 const LeftContent = styled.div`
-  height: 300px;
   width: 250px;
 `;
 
@@ -178,5 +296,29 @@ const Option = styled.p`
   font-family: "SFUIDisplay-Bold";
   font-size: 20pt;
 `;
+
+/* Logo une fois catégorie Afficher */
+const SpaceWorking = styled.span`
+  display: flex;
+  flex-direction: row;
+  margin-top: 20px;
+  /*   flex: 1; */
+`;
+
+const Space = styled.span`
+  font-family: "FallisComing";
+  color: #236857;
+  font-weight: bold;
+  font-size: 20pt;
+`;
+
+const Working = styled.span`
+  font-family: "Mont-HeavyDEMO";
+  margin-left: 3px;
+  letter-spacing: -1.3px;
+  color: #236857;
+  font-size: 23pt;
+`;
+/* ------------------------------------ */
 
 export default Presentation;
